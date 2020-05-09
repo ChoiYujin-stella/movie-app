05aa70858c09ac986f2d3871bc4bea97c1cad0de
Yujin-stella <Yujin-stella@users.noreply.github.com>
2020-05-09 21:45:33 +0900

    Updates

---
+++ precache-manifest.7667c6e7ab869a92896db622e51f9163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61333b7b97b4f92549e90f0e29f00b52",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "5cb93714bcc5b1b1d87a",
+    "url": "/movie-app/static/css/main.dcf952e9.chunk.css"
+  },
+  {
+    "revision": "f5959358a11e6d0a7660",
+    "url": "/movie-app/static/js/2.df5064f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.df5064f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb93714bcc5b1b1d87a",
+    "url": "/movie-app/static/js/main.253cc038.chunk.js"
+  },
+  {
+    "revision": "eb2afba229eb5426c33e",
+    "url": "/movie-app/static/js/runtime-main.6de98e8b.js"
+  }
+]);
